2f1f4fd24fc02cde4e53b0adcb792e18a2cb668a
torntrousers@gmail.com <ant.elder@gmail.com>
2019-10-27 10:17:41 +0000

    Add tools

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,12 @@ fn main() -> ! {
     let dp = pac::Peripherals::take().unwrap();
     let mut rcu = dp.RCU.constrain();
     let mut gpioa = dp.GPIOA.split(&mut rcu.apb2);
-    let mut pa1 = gpioa.pa1.into_push_pull_output(&mut gpioa.ctl0);
-    pa1.set_low().unwrap();
+    // let mut gpioc = dp.GPIOC.split(&mut rcu.apb2);
+    // let mut pc13 = gpioc.pc13.into_push_pull_output(&mut gpioc.ctl1);
+    // pc13.set_low().unwrap();
+    // let mut pa1 = gpioa.pa1.into_push_pull_output(&mut gpioa.ctl0);
+    // pa1.set_low().unwrap();
+    let mut pa2 = gpioa.pa2.into_push_pull_output(&mut gpioa.ctl0);
+    pa2.set_low().unwrap();
     loop {}
 }
